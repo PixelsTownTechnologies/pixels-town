ab2998c22415240d832b221220e39ecbc39380f8
PixelsTownTechnologies <pixelstown@hotmail.com>
2020-06-19 22:16:46 +0300

    Build Backend

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -106,14 +106,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 import django_heroku
 
 django_heroku.settings(locals())
